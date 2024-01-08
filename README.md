75746609d61fb4f6ad89499866952c500c15ba16
MSmith <marie.ellen@gmail.com>
2024-01-08 16:01:04 -0500

    add README and updated App.tsx and .css files

--- README.md
+++ README.md
@@ -1,30 +1,18 @@
-# React + TypeScript + Vite
+# Markdown Previewer
+This tool allows the user to enter plain text in a box with Markdown notation, and see an immediate live preview of the result to the right.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This project was built with Vite, React, TypeScript, and CSS. It uses the Marked and DOMPurify packages to process the Markdown and sanitize it again using Cross-Site Scripting (XSS).
 
-Currently, two official plugins are available:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
-## Expanding the ESLint configuration
+Markdown Guide for a quick overview of all the Markdown syntax elements:
+https://markdownguide.offshoot.io/cheat-sheet/
 
-If you are developing a production application, we recommend updating the configuration to enable type aware lint rules:
 
-- Configure the top-level `parserOptions` property like this:
 
-```js
-export default {
-  // other rules...
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['./tsconfig.json', './tsconfig.node.json'],
-    tsconfigRootDir: __dirname,
-  },
-}
-```
+From MayanWolfe's Markdown Live Preview: https://github.com/Mayanwolfe/Markdown_Live_Preview/tree/main
 
-- Replace `plugin:@typescript-eslint/recommended` to `plugin:@typescript-eslint/recommended-type-checked` or `plugin:@typescript-eslint/strict-type-checked`
-- Optionally add `plugin:@typescript-eslint/stylistic-type-checked`
-- Install [eslint-plugin-react](https://github.com/jsx-eslint/eslint-plugin-react) and add `plugin:react/recommended` & `plugin:react/jsx-runtime` to the `extends` list
+### Next version steps
+TypeScript has flagged the rawMarkup argument passed into sanitizedMarkup (App.tsx, line 30). Was able to remove the error by making createSanitizedOutput an async function. However, then the function is flagged when passed into dangerouslySetInnerHTML.
+
+Kept createSanitizedOutput as a synchronous function for now, because the program works as is, but want to figure out how to remove the error and have program run successfully.
--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,31 @@ import DOMPurify from 'dompurify';
 import './App.css';
 
 const MarkdownEditor: React.FC = () => {
-    const [markdown, setMarkdown] = useState<string>('');
+    const [markdown, setMarkdown] = useState<string>('')
 
     const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setMarkdown(e.target.value)
     }
 
+    const exportToFile = () => {
+// blob = binary large object. Creating new blob object containing markdown data
+        const blob = new Blob([markdown], {type: 'text/markdown'});
+// turn button into a link and then create a new element - anchor tag- attached to button:
+        const link = document.createElement('a');
+// set the target of that link to the file where putting stuff in, and pass in the blob:
+        link.download = 'markdown-export.md';
+        link.href = window.URL.createObjectURL(blob);
+// immediately click the link and then remove it:
+        link.click();
+        link.remove();
+    }
+
+// If change to Async function, due to TS marking 'rawMarkup' in sanitizedMarkup as an error, program does not work.
 // make the text safe [to be inputted to where it's going]
     const createSanitizedOutput = () => {
-        const rawMarkup = marked(markdown)
-        const sanitizedMarkup = DOMPurify.sanitize(rawMarkup)
-        return { __html: sanitizedMarkup }
+        const rawMarkup = marked(markdown);
+        const sanitizedMarkup = DOMPurify.sanitize(rawMarkup);
+        return { __html: sanitizedMarkup };
     }
 
     return (
@@ -22,14 +36,21 @@ const MarkdownEditor: React.FC = () => {
             <h1 className='editor-title'>Markdown Previewer</h1>
             <textarea 
             className='editor-input' 
-        // store what the user types into state:
+// store what the user types into state:
             value={markdown}
             onChange={handleInputChange}
-            placeholder='Enter Markdown here...'>
+            placeholder='Enter Markdown here...'
+            />
 
-            </textarea>
+            <div className='markdown-preview'
+// sets HTML content from markdown. Calling function to sanitize immediately:
+            dangerouslySetInnerHTML={createSanitizedOutput()}>
+            </div>
 
+            <button onClick={exportToFile} className='export-button'>Export to Markdown</button>
         </div>
     )
 }
+
+export default MarkdownEditor;
 
